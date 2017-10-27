20c5064a0c572fedb05bb42690d06fe36adc31ee
dazcha76 <dazcha@hotmail.com>
2017-10-27 13:23:09 -0700

    Created seperate JS file, added intro page and corresponding CSS

---
+++ main.js
@@ -0,0 +1,27 @@
+var the_number = null;
+
+function pick_number(){
+    the_number = Math.ceil(Math.random()*100);
+    return the_number;
+}
+
+$(document).ready(function(){
+
+    $(".start").click(function fade_intro(){
+        $("#game").css({"opacity": "1", "visibility": "visible"});
+    });
+
+    $(".shoot").click(function make_guess(){
+        var the_guess = document.getElementById("guess_input").value;
+        if(the_guess == the_number){
+            $("#response_div").text("You guessed it!");
+        } else if (the_guess > the_number){
+            $("#response_div").text("Too High!");
+        } else if(the_guess < the_number) {
+            $("#response_div").text("Too Low!");
+        }
+        the_guess = document.getElementById("guess_input").value = null;
+        the_guess = document.getElementById("guess_input").focus();
+    });
+
+});
