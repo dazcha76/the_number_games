b4482dc6121e01a2acd3834661b624e7f633a460
dazcha76 <dazcha@hotmail.com>
2018-05-08 11:20:44 -0700

    added tribute randomnizer

--- main.js
+++ main.js
@@ -3,11 +3,20 @@ let the_number = null;
 function pick_number(){
     document.getElementById("welcome").play();
     the_number = Math.ceil(Math.random()*23);
+    console.log(the_number);
     return the_number;
 }
 
 let count = 0;
 let lives = 5;
+let tributes = [
+                "images/cato.png",
+                "images/clove.png",
+                "images/glimmer.png",
+                "images/marvel.png"
+                ];
+
+let tribute_randomnizer = Math.floor(Math.random()*4);
 
 $(document).ready(function(){
 
@@ -20,7 +29,7 @@ $(document).ready(function(){
         });
         setTimeout(show_input, 4000);
 
-        cato_arrives();
+        tribute_arrives();
     });
 
     // show guess input field, katniss stops
@@ -38,7 +47,7 @@ $(document).ready(function(){
     // hide input
 
     function hide_input(){
-        // cato_leaves();
+        // tribute_leaves();
 
         $("#guess_div").css({
             opacity: 0,
@@ -50,31 +59,32 @@ $(document).ready(function(){
 
         setTimeout(show_input, 4000);
 
-        cato_arrives();
+        tribute_arrives();
     }
 
-    // cato arrives
+    // tribute arrives
 
-    function cato_arrives(){
-        var elem = document.getElementById("cato");
-        var pos = -2050;
+    function tribute_arrives(){
+        var elem = document.getElementById("tribute");
+        $("#tribute").css("background", `url(${tributes[tribute_randomnizer]})`);
+        var pos = -1800;
         var timer = setInterval(frame, -100);
         function frame() {
-            if (pos == -1120) {
+            if (pos == -1280) {
                 clearInterval(timer);
             } else {
                 pos++;
-                // elem.style.bottom = '100px';
+                elem.style.bottom = '100px';
                 elem.style.left = -pos + 'px';
             }
         }
     }
 
-    // cato leaves
+    // tribute leaves
     // have to fix flickering glitch
 
-    function cato_leaves(){
-        var elem = document.getElementById("cato");
+    function tribute_leaves(){
+        var elem = document.getElementById("tribute");
         var pos = -1000;
         var timer = setInterval(frame, -100);
         function frame() {
@@ -92,8 +102,8 @@ $(document).ready(function(){
 
     $(".shoot").click(function make_guess(){
         var the_guess = document.getElementById("guess_input").value;
+        count++;
         if(the_guess > 23 || the_guess < 1 || isNaN(the_guess)) {
-            count++;
             if(count === 5){
                 $("#response_div1").text("You lose!");
             } else {
@@ -107,7 +117,7 @@ $(document).ready(function(){
             $("#response_div1").text("You are the victor!");
             $("#response_div2").text("");
         } else if (the_guess > the_number){
-            count++;
+
             if(count === 5){
                 $("#response_div1").text("You lose!");
             } else {
@@ -117,7 +127,7 @@ $(document).ready(function(){
                 setTimeout(hide_input, 2000);
             }
         } else if(the_guess < the_number && the_guess > 0) {
-            count++;
+
             if(count === 5){
                 $("#response_div1").text("You lose!");
             } else {
